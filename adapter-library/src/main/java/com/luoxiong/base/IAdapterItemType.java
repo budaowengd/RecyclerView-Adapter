2892dbf5e01cc706b80d663b3dfba2433894b336
luoxiong <382060748@qq.com>
2016-12-12 08:03:47 +0800

    no message

--- adapter-library/src/main/java/com/luoxiong/base/IAdapterItemType.java
+++ adapter-library/src/main/java/com/luoxiong/base/IAdapterItemType.java
@@ -9,7 +9,7 @@ public interface IAdapterItemType<T> {
 
     int getItemViewLayoutId();
 
-    boolean isForViewType(T item, int position);
+
 
     void convert(ViewHolder holder, T t, int position);
 
